01ea1d2d9c4c6abb8b2137c878dc211af72a6a24
James Marrs <jamesmarrs@jmpersoallaptop.home>
2021-07-02 21:06:48 +0100

    second draft

--- js-calculator-hw/scripts/app.js
+++ js-calculator-hw/scripts/app.js
@@ -4,7 +4,7 @@
 
 // * Write your code below!
 
-const initial = window.prompt("Would you like to use the standard calculator (type 'standard') or mortgage calculator (type 'mortgage')?")
+const initial = window.prompt("Would you like to use the standard calculator (type 'standard'), mortgage calculator (type 'mortgage'), BMI (type 'bmi') or trip (type 'trip')?")
 
 if (initial === 'standard') {
 
@@ -60,7 +60,59 @@ while (mortgagecalculate) {
 
   calculate = window.confirm('Would you like to go again?')
 }
+} else if (initial === 'bmi'){
+
+  let bmi = true
+
+  while (bmi) {
+
+    const units = window.prompt("imperial or metric?")
+
+    if (units === 'imperial') {
+      const weight = parseFloat(window.prompt('what is the mass (in pounds)?'))
+      const height = parseFloat(window.prompt('what is the height (in inches)?'))      
+
+      const BMI = ((weight)/(height * height))*703;
+      window.alert('BMI is ' + BMI);      
+
+    } else if (units === 'metric'){
+      const weight = parseFloat(window.prompt('what is the mass (in kilograms)?'))
+      const height = parseFloat(window.prompt('what is the height (in metres)?'))      
+
+      const BMI = ((weight)/(height * height));
+      window.alert('BMI is ' + BMI);      
+
+    }
+  
+  }
+
+} else if (initial === 'trip') {
+ 
+  const distance = parseFloat(window.prompt('What is the distance (miles)?'))
+  let mpg = parseFloat(window.prompt('what is the fuel efficiency (mpg)?'))
+  const cpg = parseFloat(window.prompt('What is the cost per gallon?'))   
+  const speed = parseFloat(window.prompt('What is the speed (mph)?'))  
+
+  let tripTime = distance / speed;
+  tripTime = Math.round((tripTime + Number.EPSILON) * 100) / 100
+
+  if (speed > 60){
+    mpg = mpg - (2* (speed - 60));
+
+  }
+  if (mpg < 1) {
+    mpg = 1;
+  }
+
+  window.alert('mpg is' + mpg);  
+  let totalCost = (distance / mpg) * cpg; 
+  totalCost = Math.round((totalCost + Number.EPSILON) * 100) / 100
+
+
+  window.alert('Your trip will take ' + tripTime + ' hours and cost £' + totalCost + '.');     
 }
+
+
 
 
 
