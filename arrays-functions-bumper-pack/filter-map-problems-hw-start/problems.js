1a770c26e17a19c95f01b27fc635fa724911d0b9
James Marrs <jamesmarrs@jmpersoallaptop.home>
2021-07-06 12:23:17 +0100

    2nd draft

--- arrays-functions-bumper-pack/filter-map-problems-hw-start/problems.js
+++ arrays-functions-bumper-pack/filter-map-problems-hw-start/problems.js
@@ -36,38 +36,63 @@ function characterCount(array) {
 // ? eg: containsFalsey([100, {}, [], 'Mike']) => false, containsFalsey([100, {}, NaN, 'Mike', '', null]) => [NaN, '', null]
 
 function containsFalsey(array) {
-
+  return array.filter(element => !element)
 }
+
 
 // ? using any array method, write a function that returns the string elements of an array that have a given number of characters or larger
 // ? eg: wordsOfLength(['emu', 'caterpiller', 'rooster'], 4) => ['caterpiller', 'rooster']
 
 function wordsOfLength(array, minLength) {
-
+   return array.filter((word) => word.length >= minLength)
 }
+
+// const bigFruits = fruits.filter(fruit => fruit.length > 6)
+// console.log(bigFruits)
+
+// artists.find(function (artist) {
+//   return artist.length > 30
+// })
 
 // ? using any array method, write a function that converts an array of measurements as strings, into an array of numbers
 // ? eg: measurementToNumber(['10cm', '4.2cm', '205cm']) => [10, 4.2, 205]
 
 function measurementToNumber(array) {
-
+  return array.map(number => parseFloat(number))
 }
+
+// const cityShouts = cities.map(function (cityShout) {
+//   return shout = cityShout.toUpperCase() + '!'
+ 
+// })
 
 // ? using `split` and `filter` write a function that counts the number of vowels in a sentence
 // ? eg: numberOfVowels('Stay classy San Diego') => 6
 
 function numberOfVowels(string) {
+  const vowels = "aeiouAEIOU"
+
+  return string
+  .split('')
+  .filter((letter) => vowels.includes(letter)).length
 
 }
 
 // ? using, `split`, `map` and `join`, write a function that capitalises the first letter of each word in a sentance
 // ? eg: titleCase('The lord of the rings') => 'The Lord Of The Rings'
 
+
 function titleCase(string) {
+  return string
+  .split(' ')
+  .map(function(string) {
+    return string[0].toUpperCase() + string.substr(1);
+})
+.join(' ');  
 
 }
 
-
+//console.log(titleCase('The lord of the rings'))
 // ! please do not alter below ✋
 
 module.exports = {
