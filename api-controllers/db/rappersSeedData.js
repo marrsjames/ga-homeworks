1a99561ff1a855deeb16cca29e6828e9c8734651
James Marrs <jamesmarrs@jmpersoallaptop.home>
2021-09-25 11:52:43 +0100

    movies seed data now createdBy communityuser

--- api-controllers/db/rappersSeedData.js
+++ api-controllers/db/rappersSeedData.js
@@ -4,17 +4,20 @@ export const rappersSeedData = [
     AKA: 'Tony Stark',
     yearOfBirth: 1970,
     comments: [],
+    createdBy: ['614c8a3f01e9154266f238aa'],
   },
   {
     name: 'GZA',
     AKA: 'The Genius',
     yearOfBirth: 1966,
     comments: [],
+    createdBy: ['614c8a3f01e9154266f238aa'],
   },
   {
     name: 'Ol Dirty Bastard',
     AKA: 'ODB',
     yearOfBirth: 1968,
     comments: [],
+    createdBy: ['614c8a3f01e9154266f238aa'],
   },
 ]
